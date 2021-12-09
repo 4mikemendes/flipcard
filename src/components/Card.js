ce66a5cc9638c599d9f7cd30430f99ff58d177a7
mike mendes <4mikemendes@gmail.com>
2021-12-09 12:48:51 +0100

    ready to proceed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export default function Card(props) {
-    const text = props.doYouKnow ? "Ipoda" : "Goguma"
+
+    const objects = {
+        id:1,
+        question: "예쁘다",
+        reply: "beautiful"
+    }
+
+// const chosen = objects
+
+    const text = props.doYouKnow ? objects.question : objects.reply
     return (
-        <div onClick={props.handleClick}>
+        <div className="card" onClick={props.handleClick}>
             <h1>{text}</h1>
         </div>
     )
